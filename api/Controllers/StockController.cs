e1a824fdaeb3c494fe8c61b10e1491569d862b6e
kahalagan00 <privatelampas@gmail.com>
2025-01-13 16:35:41 -0800

    Daily ASP.NET learning, implemented CRUD operations for Stock and started on Comments

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -1,5 +1,9 @@
 ﻿using api.Data;
+using api.Dtos.Stock;
+using api.Interfaces;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -8,30 +12,69 @@ namespace api.Controllers
     public class StockController : ControllerBase
     {
         private readonly ApplicationDBContext _context;
-        public StockController(ApplicationDBContext context)
+        private readonly IStockRepository _stockRepo;
+        public StockController(ApplicationDBContext context, IStockRepository stockRepo)
         {
+            _stockRepo = stockRepo;
             _context = context; 
         }
 
         [HttpGet]   // OriginalRoute
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
             // Deferred execution
-            var stocks = _context.Stocks.ToList();
-            return Ok(stocks);
+            var stocks = await _stockRepo.GetAllSync();
+            var stockDto = stocks.Select(s => s.ToStockDto());
+            
+            return Ok(stockDto);
         }
 
         [HttpGet("{id}")] // OriginalRoute/id
-        public IActionResult GetById([FromRoute] int id) 
+        public async Task<IActionResult> GetById([FromRoute] int id) 
         {
-            var stock = _context.Stocks.Find(id);
+            var stock = await _stockRepo.GetByIdAsync(id);
 
             if (stock == null)
             {
                 return NotFound();
             }
 
-            return Ok(stock);
+            return Ok(stock.ToStockDto());
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
+        {
+            var stock = stockDto.ToStockFromCreateDTO();
+            await _stockRepo.CreateAsync(stock);
+            return CreatedAtAction(nameof(GetById), new { id = stock.Id }, stock.ToStockDto());
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stock = await _stockRepo.UpdateAsync(id, updateDto);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock.ToStockDto());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var stock = await _stockRepo.DeleteAsync(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
     }
 }
--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,6 @@
 using api.Data;
+using api.Interfaces;
+using api.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,15 +8,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
 builder.Services.AddControllers();
-
 builder.Services.AddOpenApi();
+
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
 
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 
 var app = builder.Build();
 
