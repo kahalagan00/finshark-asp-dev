15552ae66a82dab58b1f41353eea1d13549d4a38
kahalagan00 <privatelampas@gmail.com>
2025-01-15 15:50:42 -0800

    ASP.NET practice daily, Portfolios

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,11 +15,44 @@ namespace api.Data
 
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Portfolio>(x => x.HasKey(p => new { p.AppUserId, p.StockId }));
+
+            modelBuilder.Entity<Portfolio>()
+                .HasOne(u => u.AppUser)
+                .WithMany(u => u.Portfolios)
+                .HasForeignKey(p => p.AppUserId);
+
+            modelBuilder.Entity<Portfolio>()
+                .HasOne(u => u.Stock)
+                .WithMany(u => u.Portfolios)
+                .HasForeignKey(p => p.StockId);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = "Admin",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                },
+                new IdentityRole
+                {
+                    Id = "User",
+                    Name = "User",
+                    NormalizedName = "USER",
+                },
+
+            };
+
+            modelBuilder.Entity<IdentityRole>().HasData(roles);
+
 
             // Ensure the 'Stock' entity is mapped to the correct table name in the database
             modelBuilder.Entity<Stock>()
@@ -27,6 +61,8 @@ namespace api.Data
             // If the 'Comments' table is also problematic, you can do the same:
             modelBuilder.Entity<Comment>()
                 .ToTable("Comments"); // Optional: explicitly map if needed
+
+
         }
     }
 
